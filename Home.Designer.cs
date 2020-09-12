aea730285a78765c226ccc0703ef719ea89d9451
unknown <pritamtagare@gmail.com>
2020-09-12 21:08:01 +0530

    initial commit

---
+++ Home.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace GymManagementSystem
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newMemberToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newStaffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.equipmentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchMemberToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateMemberToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteMemberToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newMemberToolStripMenuItem,
+            this.newStaffToolStripMenuItem,
+            this.equipmentToolStripMenuItem,
+            this.searchMemberToolStripMenuItem,
+            this.updateMemberToolStripMenuItem,
+            this.deleteMemberToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(741, 27);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newMemberToolStripMenuItem
+            // 
+            this.newMemberToolStripMenuItem.Name = "newMemberToolStripMenuItem";
+            this.newMemberToolStripMenuItem.Size = new System.Drawing.Size(117, 23);
+            this.newMemberToolStripMenuItem.Text = "New Member";
+            this.newMemberToolStripMenuItem.Click += new System.EventHandler(this.newMemberToolStripMenuItem_Click);
+            // 
+            // newStaffToolStripMenuItem
+            // 
+            this.newStaffToolStripMenuItem.Name = "newStaffToolStripMenuItem";
+            this.newStaffToolStripMenuItem.Size = new System.Drawing.Size(90, 23);
+            this.newStaffToolStripMenuItem.Text = "New Staff";
+            this.newStaffToolStripMenuItem.Click += new System.EventHandler(this.newStaffToolStripMenuItem_Click);
+            // 
+            // equipmentToolStripMenuItem
+            // 
+            this.equipmentToolStripMenuItem.Name = "equipmentToolStripMenuItem";
+            this.equipmentToolStripMenuItem.Size = new System.Drawing.Size(102, 23);
+            this.equipmentToolStripMenuItem.Text = "Equipment";
+            this.equipmentToolStripMenuItem.Click += new System.EventHandler(this.equipmentToolStripMenuItem_Click);
+            // 
+            // searchMemberToolStripMenuItem
+            // 
+            this.searchMemberToolStripMenuItem.Name = "searchMemberToolStripMenuItem";
+            this.searchMemberToolStripMenuItem.Size = new System.Drawing.Size(135, 23);
+            this.searchMemberToolStripMenuItem.Text = "Search Member";
+            this.searchMemberToolStripMenuItem.Click += new System.EventHandler(this.searchMemberToolStripMenuItem_Click);
+            // 
+            // updateMemberToolStripMenuItem
+            // 
+            this.updateMemberToolStripMenuItem.Name = "updateMemberToolStripMenuItem";
+            this.updateMemberToolStripMenuItem.Size = new System.Drawing.Size(131, 23);
+            this.updateMemberToolStripMenuItem.Text = "Delete Member";
+            this.updateMemberToolStripMenuItem.Click += new System.EventHandler(this.updateMemberToolStripMenuItem_Click);
+            // 
+            // deleteMemberToolStripMenuItem
+            // 
+            this.deleteMemberToolStripMenuItem.Name = "deleteMemberToolStripMenuItem";
+            this.deleteMemberToolStripMenuItem.Size = new System.Drawing.Size(49, 23);
+            this.deleteMemberToolStripMenuItem.Text = "Exit";
+            this.deleteMemberToolStripMenuItem.Click += new System.EventHandler(this.deleteMemberToolStripMenuItem_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::GymManagementSystem.Properties.Resources.gg_logo_black_50bde9f4c8a2fbfb068ad3b72a17973bdd559cb3f54db43ae84d6ce6b8487d10;
+            this.pictureBox2.Location = new System.Drawing.Point(299, 52);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(219, 97);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GymManagementSystem.Properties.Resources.Service_02;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(690, 393);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 472);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Home";
+            this.Text = "Home";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newMemberToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newStaffToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem equipmentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchMemberToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateMemberToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteMemberToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
